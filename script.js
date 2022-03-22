6044aea06716eb5a3979774b3c4252804c4a0622
Daniel Cavalcanti <danielcavalcanti8000@gmail.com>
2022-03-22 15:46:20 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -2,20 +2,26 @@ let buttonAdd = document.getElementById('criar-tarefa');
 let inputText = document.getElementById('texto-tarefa');
 let taskList = document.getElementById('lista-tarefas');
 
+
 buttonAdd.addEventListener('click', criarTarefa);
 
 function criarTarefa() {
     let itemLista = document.createElement('li');
     itemLista.className = 'list-item'
-    itemLista.addEventListener('click', function(){
-        itemLista.style.backgroundColor = 'gray';
-    })
+    itemLista.addEventListener('click', selectItem)
     itemLista.innerText = inputText.value;
     taskList.appendChild(itemLista);
     inputText.value = '';
 }
 
-
+function selectItem(event){
+    if(document.querySelectorAll('.selected').length > 0){
+        let selectedItem = document.querySelector('.selected');
+        selectedItem.classList.remove('selected')
+    }
+    event.target.classList += ' selected';
+    console.log(event.target);
+}
 
 
 
